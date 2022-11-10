b061afb2c1e9f45758e1e205982c6b9978286459
sergioliveres <soliveres@thehotelsnetwork.com>
2022-11-10 13:22:19 +0100

    chore: setup phpunit

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_stupid()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
